d9c4cbcdb3f2cc1b0cd8ee8a25722bd25b6f9aa3
Shubhangi <shubhangithube806@gmail.com>
2024-10-02 20:29:38 +0530

    Add project files.

---
+++ GetYourPhoneWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GetYourPhoneWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
