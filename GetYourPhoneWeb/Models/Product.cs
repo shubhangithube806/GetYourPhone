fa0a2559a1e32d235b4347c0ab63c947a95bd532
Shubhangi <shubhangithube806@gmail.com>
2024-10-02 20:54:49 +0530

    Add product model.

---
+++ GetYourPhoneWeb/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace GetYourPhoneWeb.Models
+{
+    public class Product
+    {
+        public Guid ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+    }
+}
