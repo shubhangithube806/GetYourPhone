11abcfa95afa60bdd6db2b763f1ec4491ad2ffc1
Shubhangi <shubhangithube806@gmail.com>
2024-10-03 12:39:53 +0530

    Add CRUD operations for product.

--- GetYourPhoneWeb/Controllers/HomeController.cs
+++ GetYourPhoneWeb/Controllers/HomeController.cs
@@ -1,6 +1,12 @@
+using Dapper;
 using GetYourPhoneWeb.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
 using System.Diagnostics;
+using System.IO.Pipelines;
+using System.Xml.Linq;
 
 namespace GetYourPhoneWeb.Controllers
 {
@@ -8,17 +14,120 @@ namespace GetYourPhoneWeb.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private SqlConnection conn;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            conn = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
         }
 
         public IActionResult Index()
+        {
+            string query = "SELECT * FROM Product;";
+            IEnumerable<Product> productList = conn.Query<Product>(query);
+
+            return View(productList);
+        }
+
+
+        [HttpGet]
+        public IActionResult Add()
         {
             return View();
         }
 
 
+        [HttpPost]
+        public IActionResult Add(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                product.ProductId = Guid.NewGuid();
+                product.CreatedOn = DateTime.Now;
+
+                string query = $"INSERT INTO Product (ProductId, Name, Description, Price, CreatedOn) VALUES ('{product.ProductId}', '{product.Name}', '{product.Description}', '{product.Price}', '{product.CreatedOn}');";
+                conn.Execute(query);
+            
+               return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+
+        [HttpGet]
+        public IActionResult Edit(Guid? id)
+        {
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            string query = $"SELECT * FROM Product WHERE ProductId = '{id}';";
+            Product? product = conn.QueryFirstOrDefault<Product>(query);
+
+            if (product == null)
+            {
+                return NotFound();   
+            }
+
+            return View(product);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                string query = $"UPDATE Product SET Name = '{product.Name}', Description = '{product.Description}', Price = '{product.Price}', CreatedOn = '{product.CreatedOn}' WHERE ProductId = '{product.ProductId}';";
+                conn.Execute(query);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(product); 
+        }
+
+
+        [HttpGet]
+        public IActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            string query = $"SELECT * FROM Product WHERE ProductId = '{id}';";
+            Product? product = conn.QueryFirstOrDefault<Product>(query);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            string query = $"DELETE FROM Product WHERE ProductId = '{id}';";
+            conn.Execute(query);
+
+            return RedirectToAction("Index");
+        }
+
+
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- GetYourPhoneWeb/Models/Product.cs
+++ GetYourPhoneWeb/Models/Product.cs
@@ -1,13 +1,19 @@
-﻿namespace GetYourPhoneWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GetYourPhoneWeb.Models
 {
     public class Product
     {
         public Guid ProductId { get; set; }
 
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
+        [StringLength(4000)]
         public string Description { get; set; }
 
+        [Required]
         public double Price { get; set; }
 
         public DateTime CreatedOn { get; set; }
