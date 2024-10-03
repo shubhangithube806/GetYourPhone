e38fc2fa88ec09fed81a0ca9b19bacfa639dd050
Shubhangi <shubhangithube806@gmail.com>
2024-10-03 13:06:39 +0530

    Did some improvements.

--- GetYourPhoneWeb/Controllers/HomeController.cs
+++ GetYourPhoneWeb/Controllers/HomeController.cs
@@ -48,8 +48,10 @@ namespace GetYourPhoneWeb.Controllers
 
                 string query = $"INSERT INTO Product (ProductId, Name, Description, Price, CreatedOn) VALUES ('{product.ProductId}', '{product.Name}', '{product.Description}', '{product.Price}', '{product.CreatedOn}');";
                 conn.Execute(query);
-            
-               return RedirectToAction("Index");
+
+                TempData["success"] = "Product added successfully.";
+
+                return RedirectToAction("Index");
             }
 
             return View(product);
@@ -84,6 +86,8 @@ namespace GetYourPhoneWeb.Controllers
             {
                 string query = $"UPDATE Product SET Name = '{product.Name}', Description = '{product.Description}', Price = '{product.Price}', CreatedOn = '{product.CreatedOn}' WHERE ProductId = '{product.ProductId}';";
                 conn.Execute(query);
+
+                TempData["success"] = "Product updated successfully.";
 
                 return RedirectToAction("Index");
             }
@@ -122,6 +126,8 @@ namespace GetYourPhoneWeb.Controllers
 
             string query = $"DELETE FROM Product WHERE ProductId = '{id}';";
             conn.Execute(query);
+
+            TempData["success"] = "Product deleted successfully.";
 
             return RedirectToAction("Index");
         }
--- GetYourPhoneWeb/Models/Product.cs
+++ GetYourPhoneWeb/Models/Product.cs
@@ -11,9 +11,11 @@ namespace GetYourPhoneWeb.Models
         public string Name { get; set; }
 
         [StringLength(4000)]
+        [Required]
         public string Description { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "The Price field is required.")]
+        [Range(1, 1000000)]
         public double Price { get; set; }
 
         public DateTime CreatedOn { get; set; }
